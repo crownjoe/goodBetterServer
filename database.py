0e38a5e1b248fc1a2c5a70e2358297949851a2bd
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-06-15 07:37:21 +0900

    [FEAT] db 구현

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(String, primary_key=True, index=True)
